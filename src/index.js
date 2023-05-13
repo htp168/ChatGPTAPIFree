306c6975cf1b45bbe114ce1bc938e3d410bfb320
htp168 <htp168@qq.com>
2023-05-14 00:19:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import express from 'express';
 import fetch from 'node-fetch';
 
 const port = parseInt(process.env.PORT || '8080', 10);
-const api_keys = JSON.parse(process.env.API_KEYS);
+// const api_keys = JSON.parse(process.env.API_KEYS);
+const api_keys = process.env.API_KEYS;
 const upstreamUrl = 'https://api.openai.com/v1/chat/completions';
 
 const corsHeaders = {
